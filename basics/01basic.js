3c5cc3ceb008d69639784950f8e77acac564d252
Amanvishwakarma1 <145385031+Amanvishwakarma1@users.noreply.github.com>
2024-05-18 06:04:39 +0000

    string

---
+++ basics/01basic.js
@@ -0,0 +1,23 @@
+let score =33;
+console.log(typeof score)
+let valueInNumber = Number(score)
+console.log(valueInNumber)
+let sc ="aman"
+console.log(typeof sc)
+let valueInsc = Number(sc)
+console.log(valueInsc)
+//"33"=>33
+//"33abc"=>NaN
+//null=>0
+//true=>1
+let IsLoggedIn = 1
+let booleanIsLogged = Boolean(1)
+console.log(booleanIsLogged)
+//1=>true
+//0=>false
+// ""=>false
+//"aman"=>true
+let str1 = "Aman"
+let str2 = " Vishwakarma"
+let str3 =  str1+str2
+console.log(str3);
